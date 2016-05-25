d0a6d6aecf04982e923e88627f16ad44a95c3fb4
walterzea <walterzearojas@hotmail.com>
2016-05-25 07:20:38 -0600

    archivo principal del proyecto

---
+++ preyecto1.py
@@ -0,0 +1,11 @@
+  def num (a):
+  	a = int(input("ingrese un numero"))
+	if a % 2 == 2:
+		print("Es un numero par")
+	else:
+		print("Es un numero impar")
+	return num
+
+	def prob2 (F):
+	celsius = (F - 32)* 5/9
+	print(celsius)
